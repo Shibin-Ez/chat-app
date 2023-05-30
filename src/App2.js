a385970706362d3fe82e6496579805fe5cf3a49e
Shibin-Ez <118613998+Shibin-Ez@users.noreply.github.com>
2023-05-30 22:07:25 +0530

    Add files via upload

---
+++ src/App2.js
@@ -0,0 +1,119 @@
+import React, { useState, useRef } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC0grWV6P9W6ktlM29tSYcegzRzu0MDCJ0",
+  authDomain: "superchat-662e3.firebaseapp.com",
+  projectId: "superchat-662e3",
+  storageBucket: "superchat-662e3.appspot.com",
+  messagingSenderId: "215215877700",
+  appId: "1:215215877700:web:7e94ee11d4901895a31611",
+  measurementId: "G-FMZR9WS9HE"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
+
+function App() {
+
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
+      </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+    </div>
+  );
+}
+
+function SignIn(){
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return(
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  );
+}
+
+function SignOut(){
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom(){
+
+  const dummy = useRef();
+
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, {idField: 'id'});
+
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async(e) => {
+    
+    e.preventDefault();
+
+    const {uid, photoURL} = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    });
+
+    setFormValue('');
+    dummy.current.scrollIntoView({behavior: 'smooth'});
+  }
+
+  return(
+    <>
+      <main>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        <div ref={dummy}></div>
+      </main>
+      
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+        <button type="submit">Send</button>
+      </form>
+    </>
+  )
+}
+
+function ChatMessage(props){
+
+  const {text, uid, photoURL} = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (
+    <div className={`message ${messageClass}`}>    
+      <img src={photoURL} />
+      <p>{text}</p>
+    </div>
+  )
+}
+
+export default App;
